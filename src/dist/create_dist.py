d69ef8931a96274bf0cc21ad78700dd841920c78
Martin Danielsson <martin@danielsson.de>
2016-03-16 15:41:16 +0100

    Added release script for sample-repo, some changes to the sample repo.

--- src/dist/create_dist.py
+++ src/dist/create_dist.py
@@ -16,12 +16,30 @@ try:
     target_dir = os.path.join(temp_dir, 'azure-apim-deployment-utils')
     #os.makedirs(target_dir)
     
-    shutil.copytree(os.path.join('..', 'apim'), target_dir, ignore=shutil.ignore_patterns('*.pyc', 'tmp*', '.DS_*'))
+    shutil.copytree(os.path.join('..', 'apim'), target_dir, ignore=shutil.ignore_patterns('*.pyc', 'tmp*', '.DS_*', '.gitignore'))
     
-    target_dir = os.path.join(os.path.join('..', '..'), 'bin')
-    target_file = os.path.join(target_dir, 'azure-apim-deployment-utils-' + version)
-    shutil.make_archive(target_file, format='zip', root_dir=temp_dir)
+    target_zip_dir = os.path.join(os.path.join('..', '..'), 'bin')
+    target_zip_file = os.path.join(target_zip_dir, 'azure-apim-deployment-utils-' + version)
+    shutil.make_archive(target_zip_file, format='zip', root_dir=temp_dir)
+    print "Created '" + target_zip_file + ".zip'."
     
+finally:
+    shutil.rmtree(temp_dir)
+
+try:
+    temp_dir = tempfile.mkdtemp()
+    print "Using temporary directory: " + temp_dir
+    target_dir = os.path.join(temp_dir, 'sample-repo')
+    
+    shutil.copytree(
+        os.path.join(os.path.join('..', '..'), 'sample-repo'), 
+        target_dir,
+        ignore=shutil.ignore_patterns('.DS_*', '.gitignore'))
+        
+    target_zip_dir = os.path.join(os.path.join('..', '..'), 'bin')
+    target_zip_file = os.path.join(target_zip_dir, 'azure-apim-deployment-utils-sample-repo-' + version)
+    shutil.make_archive(target_zip_file, format='zip', root_dir=target_dir)
+    print "Created '" + target_zip_file + ".zip'."
 finally:
     shutil.rmtree(temp_dir)
 
