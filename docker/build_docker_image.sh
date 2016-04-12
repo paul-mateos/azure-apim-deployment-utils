c690202e03a1e663aa25ce8bfee54c07096f2b81
Martin Danielsson <martin@danielsson.de>
2016-04-12 14:51:23 +0200

    Moved docker image to official python:2.7, now works better with not-perfectly-well-formed JSON files.

--- docker/build_docker_image.sh
+++ docker/build_docker_image.sh
@@ -7,9 +7,9 @@ cd ../../docker
 cp -f ../bin/azure-apim-deployment-utils-latest-docker.zip azure-apim.zip
 
 if [ ! -z "$docker_https_proxy" ]; then
-    docker build -t donmartin76/azure-apim-deployment-utils --build-arg http_proxy=$docker_http_proxy --build-arg https_proxy=$docker_https_proxy .
+    docker build -t donmartin76/azure-apim-deployment-utils:$1 --build-arg http_proxy=$docker_http_proxy --build-arg https_proxy=$docker_https_proxy .
 else
-    docker build -t donmartin76/azure-apim-deployment-utils .
+    docker build -t donmartin76/azure-apim-deployment-utils:$1 .
 fi
 
 rm -f azure-apim.zip
